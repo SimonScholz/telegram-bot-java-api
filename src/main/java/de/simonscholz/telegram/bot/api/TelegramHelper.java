2ffe46a3f1941c743fc176acc3c4dab1158e55b8
Simon Scholz <simon.scholz@vogella.com>
2018-11-22 23:36:50 +0100

    Removed project reactor from classpath

--- src/main/java/de/simonscholz/telegram/bot/api/TelegramHelper.java
+++ src/main/java/de/simonscholz/telegram/bot/api/TelegramHelper.java
@@ -1,8 +1,9 @@
 package de.simonscholz.telegram.bot.api;
 
+import java.util.Optional;
+
 import de.simonscholz.telegram.bot.api.domain.Message;
 import de.simonscholz.telegram.bot.api.domain.Update;
-import reactor.core.publisher.Mono;
 
 public final class TelegramHelper {
 
@@ -12,20 +13,20 @@ public final class TelegramHelper {
 	 * @param update
 	 *            {@link Update}
 	 * 
-	 * @return {@link Mono} of a {@link Message}
+	 * @return {@link Optional} of a {@link Message}
 	 */
-	public static Mono<Message> getMessage(Update update) {
+	public static Optional<Message> getMessage(Update update) {
 		if (update.getMessage() != null) {
-			return Mono.just(update.getMessage());
+			return Optional.of(update.getMessage());
 		} else if (update.getEditedMessage() != null) {
-			return Mono.just(update.getEditedMessage());
+			return Optional.of(update.getEditedMessage());
 		} else if (update.getChannelPost() != null) {
-			return Mono.just(update.getChannelPost());
+			return Optional.of(update.getChannelPost());
 		} else if (update.getEditedChannelPost() != null) {
-			return Mono.just(update.getEditedChannelPost());
+			return Optional.of(update.getEditedChannelPost());
 		}
 
-		return Mono.empty();
+		return Optional.empty();
 	}
 
 	public static String getUserName(Message message) {
